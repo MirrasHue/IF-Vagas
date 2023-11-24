ab7d57b855b78267320aad28b06f8abbd09c31bd
Mirrális Santana <mirralishue@gmail.com>
2023-11-24 16:54:13 -0300

    Minor changes

--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using IF_Vagas.Data;
+using IF_Vagas.Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<StateContainer>();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>{options.UseSqlite("Data Source=Database.db");});
+builder.Services.AddScoped<ApplicationServices>();
 
 var app = builder.Build();
 
@@ -18,6 +22,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+/*var optionsBuilder = new DbContextOptionsBuilder<UserDbContext>();
+optionsBuilder.UseSqlite("Data Source = Database.db");
+using(var context = new UserDbContext(optionsBuilder.Options))  {
+        context.Database.Migrate();
+}*/
 
 app.UseHttpsRedirection();
 
