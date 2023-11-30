1dc86b3138a787ac2b356d9dc15bb8331387da24
Mirrális Santana <mirralishue@gmail.com>
2023-11-30 10:09:13 -0300

    Added placeholder data for projects

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,23 +15,31 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder){
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<User>().HasData(PopulateUsers());
-
         modelBuilder.Entity<Project>()
             .HasOne(p => p.User)
             .WithMany(u => u.Projects)
             .HasForeignKey(p => p.UserId);
 
-        //modelBuilder.Entity<Project>().HasData(PopulateProjects());
-        //modelBuilder.Entity<Vacancy>().HasData(PopulateVacancies());
+        modelBuilder.Entity<User>().HasData(GetDummyUsers());
+        modelBuilder.Entity<Project>().HasData(GetDummyProjects());
+        //modelBuilder.Entity<Vacancy>().HasData(GetDummyVacancies());
     }
 
-    private List<User> PopulateUsers(){
+    private List<User> GetDummyUsers(){
         return new List<User>{
             new User { Id = 1, Name = "Zé", Email = "ze@gmail.com", Password = "123", IsAdmin = true},
             new User { Id = 2, Name = "Maria", Email = "maria@hotmail.com", Password = "321", IsAdmin = false},
             new User { Id = 3, Name = "João", Email = "joao@bol.com", Password = "abc", IsAdmin = false},
             new User { Id = 4, Name = "Ana", Email = "ana@yahoo.com", Password = "cba", IsAdmin = true}
+        };
+    }
+
+    private List<Project> GetDummyProjects(){
+        return new List<Project>{
+            new Project { Id = 1, Name = "Project", Description = "Software", WebsiteURL = "https", UserId = 1},
+            new Project { Id = 2, Name = "Project2", Description = "DB", WebsiteURL = "https", UserId = 2},
+            new Project { Id = 3, Name = "Project3", Description = "SQL", WebsiteURL = "https", UserId = 3},
+            new Project { Id = 4, Name = "Project4", Description = "C++", WebsiteURL = "https", UserId = 4}
         };
     }
 }
